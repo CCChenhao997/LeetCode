7159f6a249cb8187aaec407ee9d7f05a8834a872
CCChenhao997 <ccchenhao997@gmail.com>
2020-02-11 14:17:14 +0800

    modify 004 hard

--- 1_Array/004.寻找两个有序数组的中位数.py
+++ 1_Array/004.寻找两个有序数组的中位数.py
@@ -34,31 +34,33 @@ class Solution:
 # 解法二
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
-
-        def getKth(nums1, start1, end1, nums2, start2, end2, k):
-            len1 = end1 - start1 + 1
-            len2 = end2 - start2 + 1
-            # 让len1的长度小于len2，这样就能保证如果有数组空了，一定是len1
-            if len1 > len2: return getKth(nums2, start2, end2, nums1, start1, end1, k)
-            # nums1数组为空
-            if len1 == 0: return nums2[start2 + k -1]
-
-            if k == 1: return float(min(nums1[start1], nums2[start2]))
-
-            # i,j 相当于双指针
-            i = start1 + min(len1, k//2) - 1
-            j = start2 + min(len2, k//2) - 1
-
-            if nums1[i] > nums2[j]:
-                return getKth(nums1, start1, end1, nums2, j+1, end2, k - (j - start2 + 1))
-            else:
-                return getKth(nums1, i+1, end1, nums2, start2, end2, k - (i - start1 + 1))
-
         n = len(nums1)
         m = len(nums2)
         left = (n+m+1) // 2
         right = (n+m+2) // 2
-        return (getKth(nums1, 0, n-1, nums2, 0, m-1, left) + getKth(nums1, 0, n-1, nums2, 0, m-1, right))*0.5
+        return (self.getKth(nums1, 0, n-1, nums2, 0, m-1, left) + self.getKth(nums1, 0, n-1, nums2, 0, m-1, right))*0.5
+    
+    def getKth(self, nums1, start1, end1, nums2, start2, end2, k):
+        len1 = end1 - start1 + 1
+        len2 = end2 - start2 + 1
+        # 让len1的长度小于len2，这样就能保证如果有数组空了，一定是len1
+        if len1 > len2: 
+            return self.getKth(nums2, start2, end2, nums1, start1, end1, k)
+        # nums1数组为空
+        if len1 == 0: 
+            return nums2[start2 + k - 1]
+
+        if k == 1: 
+            return float(min(nums1[start1], nums2[start2]))
+
+        # i,j 相当于双指针
+        i = start1 + min(len1, k//2) - 1
+        j = start2 + min(len2, k//2) - 1
+
+        if nums1[i] > nums2[j]:
+            return self.getKth(nums1, start1, end1, nums2, j+1, end2, k - (j - start2 + 1))
+        else:
+            return self.getKth(nums1, i+1, end1, nums2, start2, end2, k - (i - start1 + 1))
 
 
 
