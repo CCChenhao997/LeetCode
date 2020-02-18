f19a1203cc54df0536ee94e612ac26311b30491e
CCChenhao997 <ccchenhao997@gmail.com>
2020-02-18 14:16:42 +0800

    add 024 mid

--- 5_Linked List/024. Swap Nodes in Pairs.py
+++ 5_Linked List/024. Swap Nodes in Pairs.py
@@ -22,14 +22,58 @@ class Solution:
         p2, p3 = p1.next, p1.next.next
         
         while p2 and p3:
-            
             p1.next = p3
             p2.next = p3.next
             p3.next = p2
-        
-            # p1 = p1.next.next
+
             p1 = p2
-            p2, p3 = p1.next, p1.next.next
+            p2 = p1.next
+            if p2:
+                p3 = p2.next
             
         return dummy.next
+    
         
+        # Recursion
+        def swapPairs2(self, head: ListNode) -> ListNode:
+            # If the list has no node or has only node left.
+            if not head or not head.next:
+                return head
+            
+            # Nodes to be swapped
+            first_node = head
+            second_node = head.next
+            
+            # Swapping
+            first_node.next = self.swapPairs(second_node.next)
+            second_node.next = first_node
+            
+            return second_node
+        
+        
+        # Iteration
+        def swapPairs3(self, head: ListNode) -> ListNode:
+            # Dummy node acts as the prevNode for the head node of the list
+            # and hence stores pointer to the head node.
+            dummy = ListNode(-1)
+            dummy.next = head
+            
+            prev_node = dummy
+            
+            while head and head.next:
+                
+                # Nodes to be swapped
+                first_node = head
+                second_node = head.next
+                
+                # Swapping
+                prev_node.next = second_node
+                first_node.next = second_node.next
+                second_node.next = first_node
+                
+                # Reinitializing the head and prev_node for next swap
+                prev_node = first_node
+                head = first_node.next
+                
+            # Return the new head node.
+            return dummy.next
