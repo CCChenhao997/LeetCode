689abf743878046d23f0eee046247cce3bc81c8a
CCChenhao997 <ccchenhao997@gmail.com>
2020-02-29 11:05:30 +0800

    add 226 easy

---
+++ 4_Tree/Recursion/226. Invert Binary Tree.py
@@ -0,0 +1,58 @@
+'''
+@Description: 
+@version: 
+@Author: chenhao
+@Date: 2020-02-29 10:42:00
+@LastEditors: chenhao
+@LastEditTime: 2020-02-29 11:02:51
+'''
+
+"""
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+    
+    
+    # non-recursion
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.right)
+                stack.append(node.left)
+        return root
