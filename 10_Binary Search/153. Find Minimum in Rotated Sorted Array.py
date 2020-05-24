81333ebd0ae3e04f1650c0412a373ceca9a787bd
CCChenhao997 <ccchenhao997@gmail.com>
2020-05-24 22:47:47 +0800

    add 153 mid

---
+++ 10_Binary Search/153. Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,34 @@
+'''
+@Description: 
+@version: 
+@Author: chenhao
+@Date: 2020-05-24 22:37:44
+@LastEditors: chenhao
+@LastEditTime: 2020-05-24 22:46:17
+'''
+
+"""
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
+Find the minimum element.
+You may assume no duplicate exists in the array.
+
+Example 1:
+Input: [3,4,5,1,2] 
+Output: 1
+
+Example 2:
+Input: [4,5,6,7,0,1,2]
+Output: 0
+"""
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
