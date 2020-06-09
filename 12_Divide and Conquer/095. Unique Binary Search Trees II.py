cd0ec1bc2207ca6b4f38efe5b7f42b3716476349
CCChenhao997 <ccchenhao997@gmail.com>
2020-06-09 20:53:36 +0800

    add 095 mid

---
+++ 12_Divide and Conquer/095. Unique Binary Search Trees II.py
@@ -0,0 +1,67 @@
+'''
+@Description: 
+@version: 
+@Author: chenhao
+@Date: 2020-06-08 10:16:24
+@LastEditors: chenhao
+@LastEditTime: 2020-06-09 20:52:10
+'''
+
+"""
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+ 
+
+Constraints:
+0 <= n <= 8
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+        
+class Solution:
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        if n == 0:
+            return []
+        
+        def build_Trees(left, right):
+            all_trees = []
+            if left > right:
+                return [None]
+            for i in range(left, right + 1):
+                left_trees = build_Trees(left, i - 1)
+                right_trees = build_Trees(i + 1, right)
+                for l in left_trees:
+                    for r in right_trees:
+                        cur_tree = TreeNode(i)
+                        cur_tree.left = l
+                        cur_tree.right = r
+                        all_trees.append(cur_tree)
+            return all_trees
+        
+        res = build_Trees(1, n)
+        return res
