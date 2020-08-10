fd3c974667675a11134a164720eda05f034839a5
CCChenhao997 <ccchenhao997@gmail.com>
2020-08-10 13:31:52 +0800

    add 198 easy

---
+++ 15_Dynamic Programming/198. House Robber.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        size = len(nums)
+        if size == 1:
+            return nums[0]
+        # dp = [0] * size
+        # dp[0], dp[1]  = nums[0], max(nums[0], nums[1])
+        # for i in range(2, size):
+        #     dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+        # return dp[size-1]
+        
+        first, second = nums[0], max(nums[0], nums[1])
+        for i in range(2, size):
+            first, second = second, max(first + nums[i], second)
+        return second
+        
