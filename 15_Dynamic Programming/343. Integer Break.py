38406fdb3e96073366575a17a4170908f557016d
CCChenhao997 <ccchenhao997@gmail.com>
2020-08-14 20:30:32 +0800

    add 343 mid

---
+++ 15_Dynamic Programming/343. Integer Break.py
@@ -0,0 +1,21 @@
+"""
+Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+
+Example 1:
+Input: 2
+Output: 1
+Explanation: 2 = 1 + 1, 1 × 1 = 1.
+
+Example 2:
+Input: 10
+Output: 36
+Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+"""
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        for i in range(2, n + 1):
+            for j in range(i):
+                dp[i] = max(dp[i], j * (i - j), j * dp[i - j])
+        return dp[n]
