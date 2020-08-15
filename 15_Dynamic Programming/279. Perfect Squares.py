71ec8aa759a2462c743da17b6892883319867d85
CCChenhao997 <ccchenhao997@gmail.com>
2020-08-15 21:16:25 +0800

    add 279 mid

---
+++ 15_Dynamic Programming/279. Perfect Squares.py
@@ -0,0 +1,24 @@
+"""
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+Example 1:
+Input: n = 12
+Output: 3 
+Explanation: 12 = 4 + 4 + 4.
+
+Example 2:
+Input: n = 13
+Output: 2
+Explanation: 13 = 4 + 9.
+"""
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = i
+            j = 1
+            while i - j * j >= 0:
+                dp[i] = min(dp[i], dp[i - j * j] + 1)
+                j += 1
+        return dp[n]
