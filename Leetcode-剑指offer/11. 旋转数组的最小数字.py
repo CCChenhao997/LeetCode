aea1b0c345f4dbfd910b00b5b096d92fcc37deec
CCChenhao997 <ccchenhao997@gmail.com>
2020-12-08 12:02:28 +0800

    剑指offer

---
+++ Leetcode-剑指offer/11. 旋转数组的最小数字.py
@@ -0,0 +1,41 @@
+'''
+Description: 
+version: 
+Author: chenhao
+Date: 2020-12-08 11:35:22
+'''
+
+"""
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+
+示例 1：
+输入：[3,4,5,1,2]
+输出：1
+
+示例 2：
+输入：[2,2,2,0,1]
+输出：0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+from typing import List
+
+class Solution:
+    def minArray(self, numbers: List[int]) -> int:
+        if not numbers or len(numbers) == 0:
+            return None
+        
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            mid = (right - left) // 2 + left
+            if numbers[mid] < numbers[right]:
+                right = mid
+            elif numbers[mid] > numbers[right]:
+                left = mid + 1
+            else:
+                right -= 1
+            
+        return numbers[left]
